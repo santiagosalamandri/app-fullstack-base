38e031e6ce47fe6d6be539263e3e0bb568030674
Your Name <you@example.com>
2021-05-31 21:57:20 -0300

    Manejo de listern y request get a servidor con ajax async

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,8 +1,11 @@
-class Main{
+class Main implements EventListenerObject{
     public myFramework: MyFramework;
     public main(): void {
         console.log("Se ejecuto el metodo main!!!");
         this.myFramework = new MyFramework();
+      
+    }
+    public mostrarLista() {
         let listaUsr: Array<User> = new Array<User>();
 
         let usr1 = new User(1, "matias", "mramos@asda.com", true);
@@ -18,14 +21,58 @@ class Main{
             listaUsr[obj].printInfo();
         }
     }
+    public handleEvent(ev: Event) {
+
+        alert("Se hizo click!");
+        console.log(ev.target);
+
+        let objetoClick: HTMLElement = <HTMLElement>ev.target;
+        
+        if (objetoClick.textContent == "Listar") {
+            //this.mostrarLista();    
+            let xhr: XMLHttpRequest = new XMLHttpRequest();
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        console.log("Llego la respuesta!!!!");
+                        console.log(xhr.responseText);
+                        let parrafo = this.myFramework.getElementById("lista");
+                        parrafo.innerHTML = xhr.responseText;
+                        
+                        
+
+                        
+
+                    } else {
+                        alert("error!!")
+                    }
+                    
+                }
+                
+            }
+            xhr.open("GET","http://localhost:8000/devices",true)
+            xhr.send();
+            console.log("Ya hice el request!!")
+
+        } else {
+            alert("No hay nada que mostrar");
+        }
+        
+        
+    }
 
     
 }
-window.onload = function ejecutar() {
+window.addEventListener("load", ()=> {
     let miObjMain: Main = new Main();
     miObjMain.main();
-    let boton = miObjMain.myFramework.getElementById("boton");
-    boton.textContent="Nuevo texto"
+    let boton:HTMLElement = miObjMain.myFramework.getElementById("boton");
+    boton.textContent = "Listar";
+    boton.addEventListener("click", miObjMain);
+    
+    let btnCerrar: HTMLElement = miObjMain.myFramework.getElementById("btnCerrar");
+    btnCerrar.addEventListener("dblclick", miObjMain);
+    
 };
 
 
