6793a02b385c571ec0cbb615b1cf722bccd77598
Brian Ducca <brian.ducca@gmail.com>
2021-06-10 22:08:15 -0300

    Clase Node resuelto ejercicios

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,40 +1,48 @@
 //=======[ Settings, Imports & Data ]==========================================
 
-var PORT    = 3000;
+var PORT = 3000;
 
 var express = require('express');
-var app     = express();
-var utils   = require('./mysql-connector');
+var app = express();
+var utils = require('./mysql-connector');
 
 // to parse application/json
-app.use(express.json()); 
+app.use(express.json());
 // to serve static files
 app.use(express.static('/home/node/app/static/'));
 
-//=======[ Main module code ]==================================================
+//Ejercicio 3
+var datos = require('./datos.json');
 
-app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+//Ejercicio 4
+app.get('/devices/', function(req, res) {
+    //res.send(datos);
+    res.json(datos);
 });
-app.post('/devices/', function(req, res, next) {
-    res.send("Cambio realizado");
+
+//Ejercicio 5
+//Espera una consulta al endpoint EJ /devices/1
+//Parámetro id = el id del dispositivo a buscar
+// devuelve el dispositivo con el id que viene del parametro
+app.get('/devices/:id', function(req, res) {
+    let datosFiltrados = datos.filter(item => item.id == req.params.id);
+
+    res.json(datosFiltrados[0]);
 });
+
+//Ejercicio 6
+//Espera recibir {id:1,state:1/0} , impacta el cambio y lo devuelve
+app.post('/devices/', function(req, res) {
+    let datosFiltrados = datos.filter(item => item.id == req.body.id);
+    if (datosFiltrados.length > 0) {
+        datosFiltrados[0].state = req.body.state;
+    }
+    //res.json(datosFiltrados);
+    res.send("Todo ok");
+});
+
+
+//=======[ Main module code ]==================================================
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
 });
