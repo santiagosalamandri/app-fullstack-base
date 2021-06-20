f95b7a7f3e77eba7f9b6dc35af4680b9af5681a3
santiago salamandri <santiagosalamandri@gmail.com>
2021-06-20 15:29:04 +0100

    Agregado slider para devices tipo 1.

--- src/frontend/ts/device.ts
+++ src/frontend/ts/device.ts
@@ -1,38 +1,8 @@
-class Device {
+class Device{
 
-  private _id: number;
-  public get id(): number {
-    return this._id;
-  }
-  public set id(value: number) {
-    this._id = value;
-  }
-  private _name: string;
-  public get name(): string {
-    return this._name;
-  }
-  public set name(value: string) {
-    this._name = value;
-  }
-  private _description: string;
-  public get description(): string {
-    return this._description;
-  }
-  public set description(value: string) {
-    this._description = value;
-  }
-  private _state: boolean;
-  public get state(): boolean {
-    return this._state;
-  }
-  public set state(value: boolean) {
-    this._state = value;
-  }
-  private _type: number;
-  public get type(): number {
-    return this._type;
-  }
-  public set type(value: number) {
-    this._type = value;
-  }
+  public id: number;
+  public name: string;
+  public description: string;
+  public state: boolean;
+  public type: number;
 }
--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,87 +1,128 @@
 class Main implements EventListenerObject, HandlerPost {
     public myFramework: MyFramework;
     public myDevices: Array<Device>;
+    public deviceChanged: Device;
     public main(): void {
         console.log("Se ejecuto el metodo main!!!");
         this.myFramework = new MyFramework();
 
     }
-    public getDeviceById(id: number):Device{
+    public getDeviceById(id: number): Device {
         for (let device of this.myDevices) {
-            if (device.id == id){
-                console.log("Found device: ",device.name);
+            if (device.id == id) {
+                console.log("Found device: ", device.name);
                 return device
             }
         }
-        console.log("NOT FOUND!");    
+        console.log("NOT FOUND!");
         return null; //Not found
     }
-    public printDevices(){
+    public removeDeviceById(id: number) {
+        console.log("ANTES DE BORRAR: ");
+        this.printDevices();
+        this.myDevices.splice(id - 1, 1);
+
+        console.log("DESPUES DE BORRAR: ");
+        this.printDevices();
+
+    }
+    public printDevices() {
         for (let device of this.myDevices) {
-        console.log(device.id+" :"+device.name+" Desc:"+device.description)
+            console.log(device.id + " :" + device.name + " Desc:" + device.description)
         }
     }
     public listDevices(devices: Array<Device>) {
-        for (let disp of devices) {
+        let showSwitch = "";
+        let showSlider = "";
 
+        for (let disp of devices) {
+            console.log(disp.type);
+            if (disp.type == 0) {
+                showSwitch = "block";
+                showSlider = "none";
+            }
+            else {
+                showSwitch = "none";
+                showSlider = "block";
+            }
             let listaDisp = this.myFramework.getElementById("listaDisp");
             listaDisp.innerHTML += `<div class="col">
-            <div class="card blue-grey darken-1" >
-                <div class="card-content white-text" >
-                    <span class=card-title> ${disp.name} </span>
-                        <p> ${disp.description}.</p>
-                        <div class="switch">
-                        <label>
-                          Off
-                          <input id="disp_${disp.id}" type="checkbox">
-                          <span class="lever"></span>
-                          On
-                        </label>
-                      </div>
-                        </div>
-               
-                <div class="card-action">
-                <a id=edit_${disp.id} class="waves-effect waves-light btn " href="#modal">Editar</a>
-                <a id=del_${disp.id} class="waves-effect waves-light btn" href=#modal">Eliminar</a>
-                            </div>
-                            </div>
-                            </div>`;
+                                        <div class="card blue-grey darken-1" >
+                                            <div class="card-content white-text" >
+                                                <span class=card-title> ${disp.name} </span>
+                                                    <p> ${disp.description}.</p>
+                                                    <div class="switch" style="display:${showSwitch}">
+                                                        <label>
+                                                        Off
+                                                        <input id="disp_${disp.id}" type="checkbox">
+                                                        <span class="lever"></span>
+                                                        On
+                                                        </label>
+                                                    </div>
+                                                    <p class="range-field" style="display:${showSlider}" >
+                                                        <input type="range" id="range_${disp.id}" min="0" max="100" />
+                                                    </p>
+                                            </div>
+                                            <div class="card-action">
+                                                <a id=edit_${disp.id} class="waves-effect waves-light btn " href="#modal">Editar</a>
+                                                <a id=del_${disp.id} class="waves-effect waves-light btn" href=#modal">Eliminar</a>
+                                            </div>
+                                        </div>
+                                    </div>`;
         }
     }
-    public populateForm(id: number){
-        console.log("NUMBER: ",id);
+    public populateForm(id: number) {
         let device = this.getDeviceById(id);
-        console.log("Device: "+device.description);
-        const nameId= this.myFramework.getElementById('formName');
-        let name: HTMLInputElement = <HTMLInputElement>nameId;
-        name.value = device.name;
 
-        const formId= this.myFramework.getElementById('formDescription');
+        const nameId = this.myFramework.getElementById('formName');
+        let name: HTMLInputElement = <HTMLInputElement>nameId;
+        //console.log( "name.value "+ name.value);
+
+        name.value = device.name;
+        //console.log( "name.value "+ name.value);
+
+        const formId = this.myFramework.getElementById('formDescription');
         let desc: HTMLInputElement = <HTMLInputElement>formId;
         desc.value = device.description;
 
-
-        if(device.type == 0){
-            const typeOnOffId= this.myFramework.getElementById('typeOnOff');
+        if (device.type == 0) {
+            const typeOnOffId = this.myFramework.getElementById('typeOnOff');
             let typeOnOff: HTMLInputElement = <HTMLInputElement>typeOnOffId;
             typeOnOff.checked = true;
         }
-        else{
-            const typeDimId= this.myFramework.getElementById('typeDim');
+        else {
+            const typeDimId = this.myFramework.getElementById('typeDim');
             let typeDim: HTMLInputElement = <HTMLInputElement>typeDimId;
             typeDim.checked = true;
         }
-        /*
-        const nameId= this.myFramework.getElementById('formType');
-        let type: HTMLInputElement = <HTMLInputElement>nameId;
-        type.value = device.name;
-        console.log("NAME "+name.placeholder);
-        */
 
         const elem = this.myFramework.getElementById('modal');
-        var instance = window.M.Modal.getInstance(elem,device);
+        let instance = window.M.Modal.getInstance(elem);
         instance.open();
 
+    }
+    public getValuesFromForm() {
+        const nameId = this.myFramework.getElementById('formName');
+        let name: HTMLInputElement = <HTMLInputElement>nameId;
+        //this.deviceChanged.name=name.value;
+
+        console.log("NAME: " + name.value);
+        const formId = this.myFramework.getElementById('formDescription');
+        let desc: HTMLInputElement = <HTMLInputElement>formId;
+        //this.deviceChanged.description=desc.value;
+        console.log("DESC: " + desc.value);
+
+        const typeOnOffId = this.myFramework.getElementById('typeOnOff');
+        let typeOnOff: HTMLInputElement = <HTMLInputElement>typeOnOffId;
+        //this.deviceChanged.type=typeOnOff.checked;
+        console.log("TYPE onOff: " + typeOnOff.checked);
+  
+        const typeDimId = this.myFramework.getElementById('typeDim');
+        let typeDim: HTMLInputElement = <HTMLInputElement>typeDimId;
+        //this.deviceChanged.type=typeOnOff.checked;
+        console.log("TYPE dimm: " + typeDim.checked);
+
+        //let device:Device =new Device;
     }
     public handleEvent(ev: Event) {
 
@@ -93,21 +134,39 @@ class Main implements EventListenerObject, HandlerPost {
                 break;
             case "Editar":
                 //TODO: Abrir modal
-                let str= objetoClick.id.replace("edit_","");
+                let str = objetoClick.id.replace("edit_", "");
                 //console.log("editar " +str);
                 this.populateForm(parseInt(str));
                 break;
             case "Eliminar":
                 //console.log("eliminar " + objetoClick.id);
+                let strId = objetoClick.id.replace("del_", "");
+                this.removeDeviceById(parseInt(strId));
                 //let id = { "id": objetoClick.id }
-                this.printDevices();
+                //this.printDevices();
                 //this.myFramework.requestDELETE("http://localhost:8000/devices", this, id);
                 //TODO:enviar delete
                 break;
+            case "Confirmar":
+                console.log("se apreto Confirmar");
+                this.getValuesFromForm();
+                //TODO: Enviar cambios
+                break;
+            case "Descartar":
+                console.log("se apreto Descartar");
+                //TODO: Restaura modal
+                break;
             default:    //Checkbox clicked
                 let checkBox: HTMLInputElement = <HTMLInputElement>objetoClick;
-                console.log(objetoClick.id + " - " + checkBox.checked);
-                let datos = { "id": checkBox.id, "status": checkBox.checked }
+                var res = objetoClick.id.substring(0, 6);
+                let datos = {};
+                if(res == "range_"){
+                    datos = { "id": checkBox.id, "status": checkBox.value }
+                }
+                else{
+                    let value = checkBox.checked ? 100 : 0;
+                    datos = { "id": checkBox.id, "status": value }
+                }
                 this.myFramework.requestPOST("http://localhost:8000/devices", this, datos);
                 break;
         }
@@ -128,6 +187,8 @@ class Main implements EventListenerObject, HandlerPost {
             for (let disp of this.myDevices) {
                 let checkDisp = this.myFramework.getElementById("disp_" + disp.id);
                 checkDisp.addEventListener("click", this);
+                let rangeSwitch = this.myFramework.getElementById("range_" + disp.id);
+                rangeSwitch.addEventListener("click", this);
                 let checkEdit = this.myFramework.getElementById("edit_" + disp.id);
                 checkEdit.addEventListener("click", this);
                 let checkDel = this.myFramework.getElementById("del_" + disp.id);
@@ -157,4 +218,8 @@ window.addEventListener("load", () => {
     const elem = miObjMain.myFramework.getElementById('modal');
     const instance = this.M.Modal.init(elem, { dismissible: false });
 
+    let confirmarId = miObjMain.myFramework.getElementById("confirmarId");
+    confirmarId.addEventListener("click", miObjMain);
+    let descartarId = miObjMain.myFramework.getElementById("descartarId");
+    descartarId.addEventListener("click", miObjMain);
 });
